f7a2ff6f7685ff1a63a518b5375e622e4ddd68fd
Brayan Capet <bcapet@bcapet.jechange.ivalley>
2022-08-11 16:52:45 +0200

    yes

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ const nom1 = document.querySelector('#nom1');
 const nom2 = document.querySelector('#nom2');
 const name_button = document.querySelector('.name-button');
 const choose_name = document.querySelector('.choose-name');
+const name_card1 = document.querySelector('#top1');
+const name_card2 = document.querySelector('#top2');
+const rejouer = document.querySelector('.rejouer');
 
 
+// Gameflow module that allows me to switch between 'X' and 'O'
 const gameFlow = (() => {
     let count = 0;
 
@@ -27,6 +31,8 @@ const gameFlow = (() => {
 const gameboard = (() => {
 
     items = [];
+    player1 = '';
+    player2 = '';
 
     // Function that build my gameboard
     function build() {
@@ -41,6 +47,7 @@ const gameboard = (() => {
             items.push(div);
         }
         winning_screen_container.style.display = "none";
+        gameboard.querySelector();
     };
 
 
@@ -54,26 +61,74 @@ const gameboard = (() => {
 
     };
 
-    const chooseName = (input1, input2) => {
-        player1 = input1;
-        player2 = input2;
+    const chooseName = () => {
+        player1 = input_name1.value;
+        player2 = input_name2.value;
         nom1.textContent = player1;
         nom2.textContent = player2;
         choose_name.style.display = "none";
+        console.log(player1, player2);
+        name_card1.textContent = player1;
+        name_card2.textContent = player2;
     }
 
-    return { build }
+     // Function that put a query selector on every cell of my grid
+     const querySelector = () => {
+        console.log('querySelector function used');
+        const cell = document.querySelectorAll('.cell');
+        for(let c of cell){
+            c.addEventListener('click', function(){
+                c.textContent !== "" ? console.log('not empty') : gameFlow.implementCount() % 2 == 0 ? c.textContent = 'X' : c.textContent = 'O';
+                gameboard.winningCombination();
+            });
+        }
+        
+    }
+
+    // Function that checks if there is a winning combination or not 
+    const winningCombination = () => {
+        console.log('winningCombination used');
+        let winning  = [
+            [items[0].textContent, items[1].textContent, items[2].textContent],
+            [items[3].textContent, items[4].textContent, items[5].textContent],
+            [items[6].textContent, items[7].textContent, items[8].textContent],
+            [items[0].textContent, items[3].textContent, items[6].textContent],
+            [items[1].textContent, items[4].textContent, items[7].textContent],
+            [items[2].textContent, items[5].textContent, items[8].textContent],
+            [items[0].textContent, items[4].textContent, items[8].textContent],
+            [items[2].textContent, items[4].textContent, items[6].textContent]
+        ];
+
+        for(let win of winning){
+            if(win[0] !== "" && win[1] !== "" && win[2] !== ""){
+                if(win[0] === win[1] && win[1] === win[2]){
+                    winning_screen_container.style.display = "inline-block";
+                    rejouer.addEventListener('click', gameboard.reset);
+                }
+            }
+            else {
+                console.log('keep playing, to get a winner');
+            }
+        } 
+        
+    };
+
+    return { build, chooseName, querySelector, winningCombination, reset, player1, player2 }
 
 })();
 gameboard.build();
 
-const Player = () => {
+const Player = input => {
+    let name = input;
+    let score = 0;
+    let mark = '';
     
-    let player1 = '';
-    let player2 = '';
-
-    
-
-    return { chooseName }
+    const cocherCase = (element, mark) => {
+        element.textContent == '' ? element.textContent = mark : console.log('case déjà cochée.');
+    };
 };
-name_button.addEventListener('click', )
+
+
+name_button.addEventListener('click', function() {
+    gameboard.chooseName();
+});
--- test.js
+++ test.js
@@ -16,68 +16,6 @@ const name2 = document.querySelector('#top2');
 
 const gameboard = (() => {
 
-
-    const build = () => {
-        console.log('build function used');
-        grid.innerHTML = '';
-        for(let i = 0; i < 9; i++){
-            let div = document.createElement('div');
-            div.textContent = "";
-            div.classList.add('cell',`cell${i}`);
-            grid.appendChild(div);
-            items.push(div);
-        }
-        winning_screen_container.style.display = "none";
-        top2.innerHTML = nom2_base;
-        top1.innerHTML = nom1_base;
-        gameboard.querySelector();
-    };
-
-   
-
-    // Array that will store the cells in my tic tac toe
-    let items = [];
-
-    // Function that put a query selector on every cell of my grid
-    const querySelector = () => {
-        console.log('querySelector function used');
-        const cell = document.querySelectorAll('.cell');
-        for(let c of cell){
-            c.addEventListener('click', function(){
-                c.textContent !== "" ? console.log('not empty') : gameFlow.implementCount() % 2 == 0 ? c.textContent = 'X' : c.textContent = 'O';
-                gameboard.winningCombination();
-            });
-        }
-        rejouer.addEventListener('click', gameboard.reset);
-    }
-
-    // Function that checks if there is a winning combination or not 
-    const winningCombination = () => {
-        console.log('winningCombination used');
-        let winning  = [
-            [items[0].textContent, items[1].textContent, items[2].textContent],
-            [items[3].textContent, items[4].textContent, items[5].textContent],
-            [items[6].textContent, items[7].textContent, items[8].textContent],
-            [items[0].textContent, items[3].textContent, items[6].textContent],
-            [items[1].textContent, items[4].textContent, items[7].textContent],
-            [items[2].textContent, items[5].textContent, items[8].textContent],
-            [items[0].textContent, items[4].textContent, items[8].textContent],
-            [items[2].textContent, items[4].textContent, items[6].textContent]
-        ];
-
-        for(let win of winning){
-            if(win[0] !== "" && win[1] !== "" && win[2] !== ""){
-                if(win[0] === win[1] && win[1] === win[2]){
-                    winning_screen_container.style.display = "inline-block";
-                }
-            }
-            else {
-                console.log('keep playing, to get a winner');
-            }
-        } 
-        
-    };
-
     // Get value of an element through this function 
     const getValue = element => {
         console.log('get value function used');
@@ -86,7 +24,7 @@ const gameboard = (() => {
     };
 
 
-    return { build, winningCombination, querySelector, getValue, reset };
+
 })();
 gameboard.build();
 
